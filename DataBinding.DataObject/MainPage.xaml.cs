d9ec38e749e72aff626a082109d528d8421d47ff
Shirley Coyoc <24470067@campeche.tecnm.mx>
2025-10-29 11:18:35 -0600

    Agrega ejemplo de enlace de datos a un objeto

---
+++ DataBinding.DataObject/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using DataBinding.DataObject.Models;
+
+namespace DataBinding.DataObject
+{
+    public partial class MainPage : ContentPage
+    {
+        //_conteo lleva el conteo de la aplicacion
+        private Contador contador;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            contador = new Contador();
+            BindingContext = contador;
+           //ConteoLabel.Text = contador.Conteo.ToString();
+        }
+
+        private void OnContarButtonClicked(object sender, EventArgs e)
+        {
+            contador.Contar();
+           //ConteoLabel.Text = contador.Conteo.ToString();
+        }
+
+        private void OnReiniciarButtonClicked(object sender, EventArgs e)
+        {
+            contador.Reiniciar();
+           //ConteoLabel.Text = contador.Conteo.ToString();
+        }
+
+    }
+}
---
+++ DataBinding.DataObject/Models/Contador.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+
+namespace DataBinding.DataObject.Models
+{
+    public class Contador : INotifyPropertyChanged
+    {
+        private int _conteo;
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public int Conteo
+        {
+            get => _conteo;
+            set
+            {
+                if (_conteo != value)
+                {
+                    _conteo = value;
+                    OnPropertyChanged(nameof(Conteo));
+                }
+            }
+        }
+
+        public Contador()
+        {
+            Conteo = 0;
+        }
+
+        public void Contar()
+        {
+            Conteo++;
+        }
+
+        public void Reiniciar()
+        {
+            Conteo = 0;
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {   /*
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(
+                    this, 
+                    new PropertyChangedEventArgs(propertyName)
+                    );
+            }*/
+            //Esta es otra forma de hacer lo anterior
+            PropertyChanged?.Invoke( 
+                this, 
+                new PropertyChangedEventArgs(propertyName) 
+                );
+        }
+    }
+}
